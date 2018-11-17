19ccb1db2eeb7806b52ae1b752821873904866fc
SIVANN <44418558+SIVANNSAN@users.noreply.github.com>
2018-11-17 20:22:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js-commando");
+const Bot = new Discord.Client();
+const Token = "NTEwNzgyMjMwMjU2MTU2Njcy.DshW0g.AmK1owxxFn9MpBNCbcGx_fqU0To";
+Bot.login(Token);
+
+Bot.on("ready", function(){
+
+    console.log("Online");
+
+});
+
+Bot.on("message", function(message){
+
+    switch(message.content.toLowerCase()) {
+
+        case "sivann":
+            message.channel.send(message.author + ",\n--> **__ស៊ីវណ្ណ__** <--\n**អាយុ** : 16 ឆ្នាំ\n**រៀនថ្នាក់ទី** : 11\n**រៀននៅ** : វិទ្យាល័យជាស៊ីមរក្សជ័យ");
+        break;
+
+    }
+
+});
