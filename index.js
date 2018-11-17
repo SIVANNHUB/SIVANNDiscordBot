381e6e89fd396d72dc9c4bf8e325a79106f4b4a7
SIVANN <44418558+SIVANNSAN@users.noreply.github.com>
2018-11-17 20:24:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Bot.on("message", function(message){
     switch(message.content.toLowerCase()) {
 
         case "sivann":
-            message.channel.send(message.author + ",\n--> **__ស៊ីវណ្ណ__** <--\n**អាយុ** : 16 ឆ្នាំ\n**រៀនថ្នាក់ទី** : 11\n**រៀននៅ** : វិទ្យាល័យជាស៊ីមរក្សជ័យ");
+            message.channel.send(message.author + ",\n--> **ស៊ីវណ្ណ** <--\n**អាយុ** : 16 ឆ្នាំ\n**រៀនថ្នាក់ទី** : 11\n**រៀននៅ** : វិទ្យាល័យជាស៊ីមរក្សជ័យ");
         break;
 
     }
